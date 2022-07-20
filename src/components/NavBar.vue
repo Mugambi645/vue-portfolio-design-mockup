1f7f3e84f761d8d02a3ede56750517bcc73c194c
mugambi645 <mugambi645@gmail.com>
2022-07-20 12:18:09 +0300

    create navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+<div id="nav">
+{{title}}
+</div>
+</template>
+<script>
+export default {
+    name: "NavBar",
+    data() {
+        return {
+            title: "Hello nav"
+        }
+    }
+}
+</script>
