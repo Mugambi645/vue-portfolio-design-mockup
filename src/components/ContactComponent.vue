24f39fbad5be8efac94dd194c08729648be1fe70
mugambi645 <mugambi645@gmail.com>
2022-08-08 13:24:15 +0300

    create axios form post structure

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -3,18 +3,18 @@
 
 <div>
   <div class="contact-form-wrapper d-flex justify-content-center">
-    <form class="contact-form">
+    <form v-on:submit.prevent="submitForm" class="contact-form">
       <h5 class="title">Contact me</h5>
       <p class="description">Feel free to contact me on
       </p>
       <div>
-        <input type="text" class="form-control rounded border-white mb-3 form-input" id="name" placeholder="Name" required>
+        <input type="text" class="form-control rounded border-white mb-3 form-input" id="name" placeholder="Name" v-model="form.name" required>
       </div>
       <div>
-        <input type="email" class="form-control rounded border-white mb-3 form-input" placeholder="Email" required>
+        <input type="email" class="form-control rounded border-white mb-3 form-input" placeholder="Email" v-model="form.email" required>
       </div>
       <div>
-        <textarea id="message" class="form-control rounded border-white mb-3 form-text-area" rows="5" cols="30" placeholder="Message" required></textarea>
+        <textarea id="message" class="form-control rounded border-white mb-3 form-text-area" rows="5" cols="30" placeholder="Message" v-model="form.message" required></textarea>
       </div>
       <div class="submit-button-wrapper btn btn-primary">
         <input type="submit" value="Send">
@@ -27,12 +27,33 @@
 </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
     name: "ContactComponent",
     data() {
         return {
+          form: {
+            name: "",
+            email: "",
+            message: "",
 
+          }
         }
+    },
+    methods: {
+      submitForm() {
+        axios.post("<backend endpoint>",this.form)
+        .then((res) => {
+          //perfom success action
+          console.log(res);
+        })
+        .catch((error) => {
+          //perfom fail action
+          console.log(error);
+        }).finally(() => {
+          
+        })
+      }
     }
 }
 </script>
